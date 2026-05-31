a9b5ead6c2c575596c4b7b9ea475585dcedfe9f7
Brandon Jablasone <brandonjablasone@Brandons-MacBook-Pro.local>
2026-05-31 14:48:08 -0400

    Created the folloing file Desktop.tsx, DeskTopWorkShop.tsx, AppWindow.tsx

---
+++ src/Component/ui/DeskTopWorkSpace.tsx
@@ -0,0 +1,14 @@
+// @flow
+import * as React from 'react';
+
+// type Props = {
+//
+// };
+
+export const DeskTopWorkSpace = () => {
+    return (
+        <div className='flex flex-row w-full h-full border border-gray-950'>
+
+        </div>
+    );
+};
