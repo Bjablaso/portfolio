dcaa38ee3c67839e4597344490d7ff6cea922d3d
Brandon Jablasone <brandonjablasone@Mac.lan>
2026-06-13 16:32:05 -0400

    feat(workspace): add window workspace support

--- src/Component/ui/DeskTopWorkSpace.tsx
+++ src/Component/ui/DeskTopWorkSpace.tsx
@@ -1,13 +1,30 @@
-// @flow
-
-// type Props = {
-//
-// };
+import {Window} from "./Window.tsx";
+import {useWindowContext} from "../../Context/useWindowContext.ts";
 
 export const DeskTopWorkSpace = () => {
+    const {windowState} = useWindowContext();
+    //
     return (
-        <div className='flex flex-row w-full h-full border border-gray-950'>
+        <div className='flex flex-row w-full h-full border border-gray-950
+        relative overflow-hidden
+        '
+        >
+            {windowState.runningWindows.map((item)=>{
+                if(item.isRunning){
 
+                    return(
+                            <Window
+                                initialX={item.initialSizeX}
+                                initialY={item.initialSizeY}
+                                key={item.hash}
+                                hashNumber={item.hash}
+                            />
+                        )
+
+                }
+                return null
+            })}
+             {/*//<Window/>*/}
         </div>
     );
 };
