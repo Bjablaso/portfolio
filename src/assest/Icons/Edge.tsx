6fc39c048037b28325c0732e520e7989e02a65da
Brandon Jablasone <brandonjablasone@Mac.lan>
2026-06-13 16:32:45 -0400

    feat(icon): add Edge browser application icon

--- src/assest/Icons/Edge.tsx
+++ src/assest/Icons/Edge.tsx
@@ -1,12 +1,26 @@
 
 import * as React from "react";
+import {useWindowContext} from "../../Context/useWindowContext.ts";
 
 
 export const Edge: React.FC = () => {
+    const{windowState, dispatch}  = useWindowContext()
     return (
         <div className="flex items-center justify-center object-contain
         rounded-md hover:opacity-40
-        ">
+        "
+             onClick={() => {
+                 if(windowState.runningWindows.length >= windowState.maxWindow){
+                     return;
+                 }else {
+                     dispatch({
+                         type: "CREATE_WINDOW",
+                     })
+                 }
+
+             }
+             }
+        >
             <img   className="
                        h-4
                        h-4
