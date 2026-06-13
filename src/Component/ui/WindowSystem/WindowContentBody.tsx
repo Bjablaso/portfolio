d245fd3be729efa2339e678dcd3a4b5e3a7396a9
Brandon Jablasone <brandonjablasone@Mac.lan>
2026-06-13 16:30:44 -0400

    feat(window): implement window content body layout

---
+++ src/Component/ui/WindowSystem/WindowContentBody.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+
+
+export const WindowContentBody:React.FC=()=>{
+   //const {windowState} = useWindowContext();
+    return(
+        <div className="w-full h-full bg-[#373a3c] border-t-[0.05rem] border-t-[#f0efeb]">
+
+        </div>
+    )
+}
