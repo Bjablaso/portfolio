c45fa00d10646f8e1cc2f1f27444043da1a7e1a3
Brandon Jablasone <brandonjablasone@Mac.lan>
2026-06-25 10:44:55 -0400

    Update Edge application icon component

--- src/assest/Icons/Edge.tsx
+++ src/assest/Icons/Edge.tsx
@@ -1,33 +1,20 @@
 
 import * as React from "react";
-import {useWindowContext} from "../../Context/useWindowContext.ts";
-
+import { useWindowContext } from "../../Context/useWindowContext.ts";
 
 export const Edge: React.FC = () => {
-    const{windowState, dispatch}  = useWindowContext()
+    const { openApplication } = useWindowContext();
+
     return (
-        <div className="flex items-center justify-center object-contain
-        rounded-md hover:opacity-40
-        "
-             onClick={() => {
-                 if(windowState.runningWindows.length >= windowState.maxWindow){
-                     return;
-                 }else {
-                     dispatch({
-                         type: "CREATE_WINDOW",
-                     })
-                 }
-
-             }
-             }
+        <div
+            className="flex items-center justify-center object-contain rounded-md hover:opacity-40 cursor-pointer"
+            onClick={()=> openApplication("Edge", 280, 180)}
         >
-            <img   className="
-                       h-4
-                       h-4
-                "
-                   src="/assets/edge-svgrepo-com.svg"
-                   alt="Edge Icon"/>
-
+            <img
+                className="h-4"
+                src="/assets/edge-svgrepo-com.svg"
+                alt="Edge Icon"
+            />
         </div>
     );
 };
